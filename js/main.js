6cac1af77e19749b75c24fc491048eac5404536a
Jeremy <49862261+Unshanken@users.noreply.github.com>
2023-04-17 02:04:59 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const menuBtn = document.querySelector('.hamburger');
+const menuBtn = document.querySelector('.mobile-bar');
 const menuBar = document.querySelector('.menu-bar');
 const menuList = document.querySelector('.nav-menu');
 
 menuBtn.addEventListener('click', showMenu);
 
 function showMenu(){
-    menuBtn.classList.toggle('is-active');
-    menuBar.classList.toggle('is-active');
-    menuList.classList.toggle('is-active');
+    menuBtn.classList.toggle('active');
+    menuBar.classList.toggle('active');
+    menuList.classList.toggle('active');
 }
